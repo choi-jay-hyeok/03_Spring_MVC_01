d8f2ce22da342f13959dba46755a9994a9be0807
JAEHYEOK <tmzkdlqpffl@gmail.com>
2024-03-28 14:49:11 +0900

    섹션05_스프링MVC-시작하기(2)

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-//@Component    //스프링부트 3.0 이상부터는 클리스 레벨에 @RequestMapping이 있어도 스프링 컨트롤러로 인식하지 않음
+//@Component        //스프링부트 3.0 이상부터는 클래스 레벨에 @RequestMapping 이 있어도 스프링 컨트롤러로 인식하지 않음
 //@RequestMapping   //오직 @Controller 가 있어야 스프링 컨트롤러 인식
 public class SpringMemberFormControllerV1 {
 
